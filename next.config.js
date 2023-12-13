89740dd56c457e72cfe075b00465154b920fe3cd
Rilr20 <nrrl99@gmail.com>
2023-12-13 22:26:21 +0100

    config and more external apis

--- next.config.js
+++ next.config.js
@@ -1,7 +1,23 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lastfm.freetls.fastly.net',
+                port:'',
+                pathname: '/**',
+            },
+            {
+                protocol:'https',
+                hostname: 'steamcdn-a.akamaihd.net',
+                port: '',
+                pathname: '/**',
+            }
+        ]
+    },
     i18n: {
-        locales: ['en','se'],
+        locales: ['en', 'se'],
         defaultLocale: 'se',
     }
 }
--- src/modules/externals.js
+++ src/modules/externals.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const externals = {
     getOsrs: async function (playerName) {
         const url = "https://secure.runescape.com/m=hiscore_oldschool/index_lite.ws?player=" + playerName
@@ -9,16 +10,15 @@ const externals = {
             })
         return osrsStats
     },
-    getCarbon: async function(webUrl) {
+    getCarbon: async function (webUrl) {
         try {
             let url = "https://api.websitecarbon.com/site?url=" + webUrl;
-            console.log("url:", url);
             const response = await fetch(url);
             if (!response.ok) {
                 // throw new Error(`Failed to fetch data. Status: ${response.status}`);
                 return {
                     cleanerThan: 0.89,
-                    statistics: { co2:{ grid: { grams: 0.12} } }
+                    statistics: { co2: { grid: { grams: 0.12 } } }
                 };
             }
             const data = await response.json();
@@ -27,8 +27,35 @@ const externals = {
             console.error("Error fetching carbon data:", error);
             throw error;
         }
-    }
+    },
+    getAlbums: async function () {
+        // console.log("called");
+        // console.log(`${process.env.NEXT_PUBLIC_lastfm_username}`);
+        // const url = `http://ws.audioscrobbler.com/2.0/?method=user.getweeklyalbumchart&user=${process.env.NEXT_PUBLIC_lastfm_username}&api_key=${process.env.NEXT_PUBLIC_lastfm_api}&format=json`
+        const url = `https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=${process.env.NEXT_PUBLIC_lastfm_username}&period=7day&api_key=${process.env.NEXT_PUBLIC_lastfm_api}&limit=4&format=json`
+        const response = await fetch(url);
+        if (!response.ok) {
+            return {
+                error: "something went wrong"
+            }
+        }
+        const data = await response.json();
+        return data;
+    },
+    getSteam: async function () {
+        const url = `http://api.steampowered.com/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${process.env.NEXT_PUBLIC_steam_api}&steamid=${process.env.NEXT_PUBLIC_steam_id}&count=2&format=json`
+        const response = await fetch(url);
+        if (!response.ok) {
+            return {
+                error: "something went wrong"
+            }
+        }
+        const data = await response.json();
+        return data;
+    },
 }
 export default externals;
-export const getOsrs = externals.getOsrs
-export const getCarbon = externals.getCarbon
+export const getOsrs = externals.getOsrs;
+export const getCarbon = externals.getCarbon;
+export const getAlbums = externals.getAlbums;
+export const getSteam = externals.getSteam;
