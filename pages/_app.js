0d264d3675eece1951663cd1b74aa778b92acea7
Rilr20 <nrrl99@gmail.com>
2023-12-19 00:59:00 +0100

    localisation and added header footer

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,23 @@
 import '../src/styles/css/app.min.css'
+import { NextIntlClientProvider } from 'next-intl';
+import Header from "@/components/header";
+import Footer from "@/components/footer";
+import { useRouter } from 'next/router';
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps }, }) {
-
+    const router = useRouter();
+    console.log(router.locale);
     return (
-        <div>
-            <Component {...pageProps} />
-        </div>
-
+        <NextIntlClientProvider
+            locale={router.locale}
+            timeZone="Europe/Stockholm"
+            messages={pageProps.messages}
+        >
+            <div>
+                <Header locale={router.locale} />
+                <Component {...pageProps} />
+                <Footer />
+            </div>
+        </NextIntlClientProvider>
     )
 }
