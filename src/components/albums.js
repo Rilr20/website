c4035fc5f796ba76e97883f918bd75f2d1a30965
Rilr20 <nrrl99@gmail.com>
2023-12-19 01:04:33 +0100

    template img

--- src/components/albums.js
+++ src/components/albums.js
@@ -7,9 +7,7 @@ export default function Albums({ albums }) {
             {
                 albums.topalbums.album.map(function (album, key) {
                     return <div key={key} className="album" >
-                        <Image src={album.image[2]["#text"]} objectFit='contain' sizes="100vw" width={0} height={0} alt={album.name} className="album-art" style={{ minWidth:"160px", width: 'auto', height: 'auto', maxWidth:"160px" }} />
-                        {/* <p>{album.playcount} playcount</p> */}
-                        {/* <p>rank: {album["@attr"].rank}</p> */}
+                        <Image src={album.image[2]["#text"] ? album.image[2]["#text"] : '/img/160x160.webp'} objectFit='contain' sizes="100vw" width={0} height={0} alt={album.name} className="album-art" style={{ minWidth:"160px", width: 'auto', height: 'auto', maxWidth:"160px" }} />
                         <p className="album-title">{album.name.replace(/\[.*?\]/g, '')}</p>
                     </div>
                 })
