fe0445dd9ef06f407db4b945f14880505ef38355
Rilr20 <nrrl99@gmail.com>
2023-12-29 11:03:18 +0100

    dummy img

--- src/components/steam.js
+++ src/components/steam.js
@@ -5,8 +5,8 @@ import Image from "next/image";
 export default function Steam({ steam }) {
     return (
         <div className='steam-game'>
-            <Image src={`https://steamcdn-a.akamaihd.net/steam/apps/${steam.response.games[0].appid}/header.jpg`} alt="game header" width="160" height="75" />
-            <Image src={`https://steamcdn-a.akamaihd.net/steam/apps/${steam.response.games[1].appid}/header.jpg`} alt="game header" width="160" height="75" />
+            <Image src={steam.response.games[0].appid === "" ? '/img/160x75.webp' : `https://steamcdn-a.akamaihd.net/steam/apps/${steam.response.games[0].appid}/header.jpg`} alt="game header" width="160" height="75" />
+            <Image src={steam.response.games[1].appid === "" ? '/img/160x75.webp' : `https://steamcdn-a.akamaihd.net/steam/apps/${steam.response.games[1].appid}/header.jpg`} alt="game header" width="160" height="75" />
         </div>
     )
 }
