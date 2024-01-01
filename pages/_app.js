c85cb6d29dac7b1ea541bcf87c4cbfed6b03eef3
Rilr20 <nrrl99@gmail.com>
2024-01-01 16:49:52 +0100

    website title

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,19 @@ import { NextIntlClientProvider } from 'next-intl';
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { useRouter } from 'next/router';
+import Head from 'next/head'
 
 export default function MyApp({ Component, pageProps: { session, ...pageProps }, }) {
     const router = useRouter();
-    console.log(router.locale);
     return (
         <NextIntlClientProvider
             locale={router.locale}
             timeZone="Europe/Stockholm"
             messages={pageProps.messages}
         >
+            <Head>
+                <title>RILR20</title>
+            </Head>
             <div>
                 <Header locale={router.locale} />
                 <Component {...pageProps} />
