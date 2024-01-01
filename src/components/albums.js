ece550d419d0a65c72fadee7e3a081c338ee018c
Rilr20 <nrrl99@gmail.com>
2024-01-01 16:57:54 +0100

    image fixes

--- src/components/albums.js
+++ src/components/albums.js
@@ -7,7 +7,7 @@ export default function Albums({ albums }) {
             {
                 albums.topalbums.album.map(function (album, key) {
                     return <div key={key} className="album" >
-                        <Image src={album.image[2]["#text"] ? album.image[2]["#text"] : '/img/160x160.webp'} objectFit='contain' sizes="100vw" width={0} height={0} alt={album.name} className="album-art" style={{ minWidth:"160px", width: 'auto', height: 'auto', maxWidth:"160px" }} />
+                        <Image src={album.image[2]["#text"] ? album.image[2]["#text"] : '/img/160x160.webp'} sizes="100vw" width={0} height={0} alt={album.name} className="album-art" style={{ minWidth: "160px", width: 'auto', height: 'auto', maxWidth: "160px", objectFit: "contain" }} />
                         <p className="album-title">{album.name.replace(/\[.*?\]/g, '')}</p>
                     </div>
                 })
