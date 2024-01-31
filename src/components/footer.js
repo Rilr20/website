e838a6e9c8868414fb668d7d1086362e774a84ee
Rilr20 <nrrl99@gmail.com>
2024-01-31 13:19:36 +0100

    footer year automatically updates

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 export default function Footer() {
+  let date = new Date().getFullYear();
   return (
     <footer>
           <div></div>
-          <div><p className='centre copyright'>&copy; 2023 RILR20</p></div>
+      <div><p className='centre copyright'>&copy;{date} RILR20</p></div>
     </footer>
   )
 }
